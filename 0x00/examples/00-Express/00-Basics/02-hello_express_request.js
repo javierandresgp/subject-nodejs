af6d92f7e4e6cd8494b72819b7cea35c6a78faa2
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-04 16:55:55 -0500

    Training

---
+++ 0x00/examples/00-Express/00-Basics/02-hello_express_request.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require("express"),
+  app = express();
+app.get("/", (req, res) => {
+  res.end("<h1>Hello, World! from 02-Express</h1>");
+});
+app.get("/user/:name", (req, res) => {
+  res.end(`<h2>Hello, ${req.params.name}!</h2>`);
+});
+app.listen(3000);
+console.log("Starting Express in the port 3000");
