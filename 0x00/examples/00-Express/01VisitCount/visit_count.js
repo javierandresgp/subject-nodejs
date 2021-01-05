3bab78537d8afa0b1fbf42b92b8ddab6ef8c43ee
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-05 14:49:10 -0500

    Training

--- 0x00/examples/00-Express/01VisitCount/visit_count.js
+++ 0x00/examples/00-Express/01VisitCount/visit_count.js
@@ -6,7 +6,9 @@ const express = require("express"),
 app.use(cookieParser());
 app.use(cookieSession({ secret: "secreto" }));
 app.get("/", (req, res) => {
-  res.send(`<h1>Hello, World!; from Express-Visit count</h1>`);
+  req.session.visits || (req.session.visits = 0);
+  let v = req.session.visits++;
+  res.send(`<h1>Hello, World!; from Express-Visit count<br>Visits: ${v}</h1>`);
 });
 app.listen(3000);
 console.log("Server running in localhost, port 3000");
