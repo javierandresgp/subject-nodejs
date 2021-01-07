9693e8bec2c13f5edc4aa735f2cf62f8727d95ef
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-07 11:15:36 -0500

    Training

--- 0x00/examples/03TemplatesEngines/app.js
+++ 0x00/examples/03TemplatesEngines/app.js
@@ -3,6 +3,7 @@
 const express = require("express"),
   //path = require("path"),
   favicon = require("serve-favicon"),
+  morgan = require("morgan"),
   jade = require("jade"),
   routes = require("./routes/index"),
   faviconUrl = `${__dirname}/public/img/favicon.ico`,
@@ -17,6 +18,7 @@ app
   .set("port", port)
   // Execute middlewares
   .use(favicon(faviconUrl))
+  .use(morgan("dev"))
   .use(publicDir)
   // Execute the router middleware
   .use("/", routes);
