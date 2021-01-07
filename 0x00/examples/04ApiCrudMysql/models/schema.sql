15f744688a9d72244d89939dfb6d54f22193fb0d
Javier Andrés <55107141+JA-programmer@users.noreply.github.com>
2021-01-07 12:33:01 -0500

    Training

---
+++ 0x00/examples/04ApiCrudMysql/models/schema.sql
@@ -0,0 +1,10 @@
+DROP DTABASE IF EXIST movies;
+CREATE DATABASE IF NOT EXIST movies;
+USE movies;
+CREATE TABLE IF NOT EXIST movie(
+  movie_id VARCHAR(9) PRIMARY KEY,
+  title VARCHAR(100),
+  release VARCHAR(4),
+  rating DECIMAL(2, 1),
+  image VARCHAR(255)
+);
